3ab8e63877b91e69d88bf7ce71bb4195145345f6
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-04-06 23:26:00 -0300

    update DefaultInput component to accept labelText prop for dynamic labeling

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -3,17 +3,18 @@ import styles from "./styles.module.css";
 type InputType = {
   type: "text" | "number";
   id: string;
+  labelText: string;
 };
 
 // Pegar os tipos do input
 // type InputType = {} & React.ComponentProps<'input'>;
 // Dessa forma eu consigo pegar todos os tipos que um input permite
 
-export function DefaultInput({ type, id }: InputType) {
+export function DefaultInput({ type, id, labelText }: InputType) {
   return (
     <>
       <label className={styles.labelTask} htmlFor={id}>
-        Task
+        { labelText }
       </label>
       <input id={id} className={styles.inputTask} type={type} />
     </>
