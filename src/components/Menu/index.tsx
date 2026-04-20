1b48103675ce2c23c784df2ba5c7531f86be240e
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-04-20 16:00:14 -0300

    implement theme toggle functionality in Menu component; update HTML language attribute and add light theme styles

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,23 +1,54 @@
 import { CogIcon, HouseIcon, SunIcon, TimerResetIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState } from "react";
+
+type ThemePage = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<ThemePage>("dark");
+
+  function handleClickTheme(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
   return (
     <>
       <nav className={styles.menu}>
-        <a className={styles.menuLink} href="#">
+        <a className={styles.menuLink} href="#" aria-label="Home" title="Home">
           <HouseIcon />
         </a>
 
-        <a className={styles.menuLink} href="#">
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Histórico"
+          title="Histórico"
+        >
           <TimerResetIcon />
         </a>
 
-        <a className={styles.menuLink} href="#">
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Configurações"
+          title="Configurações"
+        >
           <CogIcon />
         </a>
 
-        <a className={styles.menuLink} href="#">
+        <a
+          className={styles.menuLink}
+          href="#"
+          aria-label="Tema"
+          title="Tema"
+          onClick={handleClickTheme}
+        >
           <SunIcon />
         </a>
       </nav>
